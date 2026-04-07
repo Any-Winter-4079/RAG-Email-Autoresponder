58d746bed6e477aaf6b1db725cb19af0889e0239
Any-Winter-4079 <50542132+Any-Winter-4079@users.noreply.github.com>
2026-04-07 19:13:27 +0200

    add deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 # usage: ./deploy.sh
 
-echo ">>> 1. Deploying decoder service (GPU)..."
-modal deploy services/decoder.py
+echo ">>> 1. Deploying legacy decoder service (GPU)..."
+modal deploy services/decoder_legacy.py
 
-echo ">>> 2. Deploying encoder upserter or updater service (CPU)..."
-modal deploy services/encoder_cpu_upserter_or_updater.py
+echo ">>> 2. Deploying latest decoder service (GPU)..."
+modal deploy services/decoder_latest.py
 
-echo ">>> 3. Deploying encoder retriever service (CPU)..."
-modal deploy services/encoder_cpu_retriever.py
+echo ">>> 3. Deploying encoder service (CPU)..."
+modal deploy services/encoder_cpu.py
 
-echo ">>> 4. Deploying encoder upserter or updater service (GPU)..."
-modal deploy services/encoder_gpu_upserter_or_updater.py
+echo ">>> 4. Deploying encoder service (GPU)..."
+modal deploy services/encoder_gpu.py
 
-echo ">>> 5. Deploying encoder retriever service (GPU)..."
-modal deploy services/encoder_gpu_retriever.py
-
-echo ">>> 6. Deploying collection handler service..."
+echo ">>> 5. Deploying collection handler service..."
 modal deploy services/collection_handler.py
+
+echo ">>> 6. Deploying LLM judge service..."
+modal deploy services/llm_judge.py
 
 echo ">>> 7. Deploying crawler agent (yearly cron)..."
 modal deploy services/crawler_agent.py
